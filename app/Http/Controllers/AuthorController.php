f424f52de802acd4b74793cd328beabb4cdf8501
Patricio Aumedes <patricioaumedes@gmail.com>
2018-06-24 18:22:37 -0300

    Seeders replacated

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Author;
 
 class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Author::all();
+        return view('authors', compact('authors'));
+    }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use App\Book;
+use App\Http\Requests\BooksRequest;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(BooksRequest $request)
+    {   
+        
+        Book::create($request->all());
+        return redirect('/books');
+    }
+
+    public function edit(Book $book)
+    {
+        // $book = Book::find($id);
+        return view('books.edit', compact('book'));
+    }
+
+    public function update(BooksRequest $request, $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->fill($request->all());
+        $book->save();
+        return redirect('/books');
+    }
 }
